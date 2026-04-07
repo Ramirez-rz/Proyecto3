c309fe9d2eff076f4ca322ac25e476752dbaabf7
AndresRmz <andresrmzruiz@gmail.com>
2026-04-06 19:56:56 -0600

    Primer commit

---
+++ src/components/LeftControl.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function LeftControl() {
+  return (
+    <div className="w-[100px] h-[200px] border-4 border-solid ">LeftControl</div>
+  )
+}
+    
---
+++ src/components/RightControl.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RightControl() {
+  return (
+    <div className="w-[100px] h-[200px] border-4 border-solid ">RightControl</div>
+  )
+}
